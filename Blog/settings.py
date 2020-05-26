fa448fea66805723529597cb4bda4f05a17f6e83
ToniIvars <toniivaju@gmail.com>
2020-05-26 18:08:39 +0200

    Añadida una view para el error 404

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'eu09(ilk6@4sfdofb=b_2ht@vad*$ehh9-3u_83+y%(+phh)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1', 'elbloglibre.herokuapp.com']
 
